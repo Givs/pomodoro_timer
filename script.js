4744516f092243aec6f8fb70d398428dc125fec2
Givaldo Neto <valdingivs@outlook.com>
2022-12-12 21:15:31 -0300

    feat: add timer decrement function

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const stop_btn = document.querySelector('.stop')
 const set_btn = document.querySelector('.set')
 const soundOn_btn = document.querySelector('.sound-on')
 const soundOff_btn = document.querySelector('.sound-off')
+const minutesDisplay = document.querySelector('.minutes')
+const secondsDisplay = document.querySelector('.seconds')
+
+let minutes
 
 function hideAndShow(hide, show, secondeHide, secondShow){
     hide.classList.add('hide')
@@ -16,8 +20,33 @@ function hideAndShow(hide, show, secondeHide, secondShow){
     
 }
 
+function countDown(){
+    setTimeout(() => {
+        let seconds = Number(secondsDisplay.textContent)
+        let minutes = Number(minutesDisplay.textContent)
+
+        if (seconds <= 0){
+            seconds = 60
+
+            minutesDisplay.textContent = minutesDisplay.textContent <= 10 ? String(minutes - 1).padStart(2, '0') : minutes - 1
+        }
+
+        secondsDisplay.textContent = secondsDisplay.textContent <= 10 ? String(seconds - 1).padStart(2, '0') : seconds - 1
+        
+        if (minutesDisplay.textContent <= 0 && secondsDisplay.textContent <= 0){
+            hideAndShow(stop_btn, set_btn, pause_btn, play_btn)
+            return 
+        }
+
+        countDown()
+    }, 1000)
+}
+
 play_btn.addEventListener('click', () => {
-    hideAndShow(play_btn, pause_btn, set_btn, stop_btn)
+    if(minutesDisplay.textContent != '00' || secondsDisplay.textContent != '00'){
+        hideAndShow(play_btn, pause_btn, set_btn, stop_btn)
+        countDown()
+    } 
 })
 
 pause_btn.addEventListener('click', () => {
@@ -34,4 +63,11 @@ soundOn_btn.addEventListener('click', () => {
 
 soundOff_btn.addEventListener('click', () => {
     hideAndShow(soundOff_btn, soundOn_btn)
+})
+
+set_btn.addEventListener('click', () => {
+    minutes = prompt('Quantos minutos? ')
+    if (minutes){
+        minutesDisplay.textContent = String(minutes).padStart(2, '0')
+    }
 })
